f8e25b5a408077a873812811c77e4bba67e0a6c9
antarus66 <antarus66@gmail.com>
2015-08-05 20:50:02 +0300

    hot-fix-hash-navigation - Fixed page reloading.

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ define(['marionette', 'bootstrap', 'validation-model'], function (Marionette) {
     App.on('start', function () {
         require(['routes'], function () {
             if (Backbone.history) {
-                Backbone.history.start({ pushState: true });
+                Backbone.history.start();
             }
         });
     });
--- public/js/routes.js
+++ public/js/routes.js
@@ -23,16 +23,13 @@ define(['app'], function (App) {
                 });
             },
             questions: function () {
-/*                if(!App.User){
+                if(!App.User){
                     Backbone.history.navigate('login', { trigger: true })
                 }else{
                     require(['controllers/question'], function (controller) {
                         controller.questions();
                     });
-                }*/
-                require(['controllers/question'], function (controller) {
-                    controller.questions();
-                });
+                }
             },
             question: function (id) {
                 require(['controllers/question'], function (controller) {
