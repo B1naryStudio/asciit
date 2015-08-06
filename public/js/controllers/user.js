1c33ed69a547de3392f96a0fdb824e6b5078f7de
devilrep <flaringrep@gmail.com>
2015-08-06 12:31:23 +0300

    Hot fix: move includes model into dependencies

--- public/js/controllers/user.js
+++ public/js/controllers/user.js
@@ -1,31 +1,29 @@
-define(['app', 'views/user/login'], function (App, View) {
+define(['app', 'views/user/login', 'models/user'], function (App, View) {
     App.module('User', function (User, App, Backbone, Marionette, $, _) {
         var Controller = Marionette.Controller.extend({
             login: function () {
-                require(['models/user'], function () {
-                    var view = new View();
-                    App.Main.Layout.getRegion('content').show(view);
-                    App.trigger('popup:show', {
-                        header: {
-                            title: 'Please log in',
-                            without_close: true
-                        },
-                        class: 'login',
-                        contentView: view,
-                        modal: {
-                            keyboard: false,
-                            backdrop: 'static'
-                        }
-                    });
-                    User.Controller.listenTo(view, 'form:submit', function (data) {
-                        $.when(App.request('user:login', data.email, data.password)).done(function (model) {
-                            App.User.Current = model;
-                            App.trigger('question:collection');
-                            App.trigger('popup:close');
-                            Backbone.history.navigate('/', { trigger: true })
-                        }).fail(function (errors) {
-                            view.triggerMethod('data:invalid', errors);
-                        });
+                var view = new View();
+                App.Main.Layout.getRegion('content').show(view);
+                App.trigger('popup:show', {
+                    header: {
+                        title: 'Please log in',
+                        without_close: true
+                    },
+                    class: 'login',
+                    contentView: view,
+                    modal: {
+                        keyboard: false,
+                        backdrop: 'static'
+                    }
+                });
+                User.Controller.listenTo(view, 'form:submit', function (data) {
+                    $.when(App.request('user:login', data.email, data.password)).done(function (model) {
+                        App.User.Current = model;
+                        App.trigger('question:collection');
+                        App.trigger('popup:close');
+                        Backbone.history.navigate('/', { trigger: true })
+                    }).fail(function (errors) {
+                        view.triggerMethod('data:invalid', errors);
                     });
                 });
             },
