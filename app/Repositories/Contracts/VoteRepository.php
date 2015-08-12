0a7fc2086a955adc255f638a943cdae35e630053
antarus66 <antarus66@gmail.com>
2015-08-12 12:25:37 +0300

    votes: Added repository

---
+++ app/Repositories/Contracts/VoteRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+/**
+ * Interface VoteRepository
+ * @package namespace App\Repositories\Repositories;
+ */
+interface VoteRepository extends RepositoryInterface
+{
+    //
+}
---
+++ app/Repositories/Entities/Vote.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class Vote extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    protected $fillable = ['sign', 'user_id', 'q_and_a_id'];
+
+    protected $timestamps = false;
+
+    public function user()
+    {
+        return $this->belongsTo('App\Repositories\Entities\User');
+    }
+}
