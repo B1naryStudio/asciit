dfe6967af9a63317c3fa97e90e07c461681457c4
antarus66 <antarus66@gmail.com>
2015-08-15 15:24:48 +0300

    code cleanup: Cleaned controller methods and routes

--- app/Http/Controllers/API/Question/AnswerController.php
+++ app/Http/Controllers/API/Question/AnswerController.php
@@ -32,16 +32,6 @@ class AnswerController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  Request  $request
@@ -60,50 +50,5 @@ class AnswerController extends Controller
         }
 
         return Response::json($answer->toArray(), 201, [], JSON_NUMERIC_CHECK);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int  $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
