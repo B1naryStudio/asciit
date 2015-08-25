6b5e2a8e318078a393dcb332094840f9303a06a2
antarus66 <antarus66@gmail.com>
2015-08-26 00:26:43 +0300

    hot-fix-select-localisation: Plugged in the lang options of select2 and CKEditor

--- public/js/views/tag/select.js
+++ public/js/views/tag/select.js
@@ -17,51 +17,59 @@ define([
             className: 'tag-select',
             childView: View.TagSelectRow,
             onShow: function () {
-                this.$el.attr('name', 'tag')
-                    .attr('multiple', 'multiple')
-                    .select2({
-                        placeholder: i18n.t('tags.select'),
-                        tags: true,
-                        ajax: {
-                            url: App.prefix + '/api/v1/tags',
-                            dataType: 'json',
-                            delay: 250,
-                            data: function (params) {
-                                return {
-                                    search: params.term,
-                                    page_size: 10
-                                };
+                var self = this;
+                var lang = i18n.lng();
+
+                require(['vendor/select2/i18n/' + lang], function () {
+                    self.$el.attr('name', 'tag')
+                        .attr('multiple', 'multiple')
+                        .select2({
+                            placeholder: i18n.t('tags.select'),
+                            language: lang,
+                            tags: true,
+                            ajax: {
+                                url: App.prefix + '/api/v1/tags',
+                                dataType: 'json',
+                                delay: 250,
+                                data: function (params) {
+                                    return {
+                                        search: params.term,
+                                        page_size: 10
+                                    };
+                                },
+                                processResults: function (data, params) {
+                                    for (var i = 0; i < data.length; i++ ) {
+                                        data[i].text = data[i]['title'];
+                                        data[i].id = data[i]['title'];
+                                    }
+                                    return {
+                                        results: data
+                                    };
+                                },
+                                cache: true
                             },
-                            processResults: function (data, params) {
-                                for (var i = 0; i < data.length; i++ ) {
-                                    data[i].text = data[i]['title'];
-                                    data[i].id = data[i]['title'];
+                            escapeMarkup: function (markup) {
+                                return markup;
+                            },
+                            minimumInputLength: 2,
+
+                            templateResult: function (repo) {
+                                if (repo.loading) {
+                                    return repo.text;
                                 }
-                                return {
-                                    results: data
-                                };
+
+                                if (!repo.title) {
+                                    repo.title = repo.text;
+                                }
+                                return SelectRowTpl(repo);
                             },
-                            cache: true
-                        },
-                        escapeMarkup: function (markup) {
-                            return markup;
-                        },
-                        minimumInputLength: 2,
-
-                        templateResult: function (repo) {
-                            if (repo.loading) {
-                                return repo.text;
+                            templateSelection: function (repo) {
+                                return repo.title || repo.text;
                             }
+                        }).val(null).trigger('change');
+                });
 
-                            if (!repo.title) {
-                                repo.title = repo.text;
-                            }
-                            return SelectRowTpl(repo);
-                        },
-                        templateSelection: function (repo) {
-                            return repo.title || repo.text;
-                        }
-                    }).val(null).trigger('change');
+
             }
         });
     });
