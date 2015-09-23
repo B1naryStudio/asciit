cf098ea20ef42ebc8c57036b05b9a4ccedfc6957
antarus66 <antarus66@gmail.com>
2015-09-23 13:50:42 +0300

    third-party-code: Added server validation of the gist path

--- app/Http/Controllers/API/CodeSnippetController.php
+++ app/Http/Controllers/API/CodeSnippetController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 
 class CodeSnippetController extends Controller
 {
@@ -18,7 +19,7 @@ class CodeSnippetController extends Controller
      */
     public function getGistWidget(Request $request, DataGrabber $grabber)
     {
-        $link = $request->get('link') . '.json';
+        // Getting a prefix and jsPath
         $prefix = env('SERVER_PREFIX', '');
         $jsPath = url(($prefix ? '/' : '' ) . env('JS_PATH'));
 
@@ -28,15 +29,36 @@ class CodeSnippetController extends Controller
             $prefixToPaste = '';
         }
 
+        // Validating the input
+        $validator = Validator::make(['link' => $request->get('link')], [
+            'link' => [
+                'required',
+                'regex:/(ftp|http|https):\/\/gist.github.com\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/i'
+            ]
+        ]);
+
+        if ($validator->fails()) {
+            return Response::view('errors.gist.404', [
+                'js_path'  => $jsPath,
+                'prefix'  => $prefixToPaste,
+                'description' => 'Incorrect Gist path'
+            ],  404);
+        }
+
+        // Trying to get a data from remote server
+        $link = $request->get('link') . '.json';
+
         try {
             $data = $grabber->getFromJson($link);
         } catch (RemoteDataGrabberException $e) {
             return Response::view('errors.gist.404', [
                 'js_path'  => $jsPath,
                 'prefix'  => $prefixToPaste,
+                'description' => 'GitHub Gist is not found'
             ],  404);
         }
 
+        // Successfull answer with gist
         return Response::view('widgets.gist', [
             'stylesheet_link' => $data->stylesheet,
             'snippet'         => $data->div,
