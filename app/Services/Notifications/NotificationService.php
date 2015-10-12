a4a13f653764934c8dac814fe30529fe871415ab
antarus66 <antarus66@gmail.com>
2015-10-13 01:19:05 +0300

    hot-fix-notifications-url: Refactored a notification query sending.

--- app/Services/Notifications/NotificationService.php
+++ app/Services/Notifications/NotificationService.php
@@ -3,11 +3,19 @@
 namespace App\Services\Notifications;
 
 use App\Services\Notifications\Contracts\NotificationServiceInterface;
+use App\Services\RemoteDataGrabber\Exceptions\RemoteServerException;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Log;
+use App\Services\RemoteDataGrabber\Contracts\DataGrabberInterface;
 
 class NotificationService implements NotificationServiceInterface
 {
+    protected $dataGrabber;
+
+    public function __construct(DataGrabberInterface $dataGrabber) {
+        $this->dataGrabber = $dataGrabber;
+    }
+
     public function send(array $data)
     {
         $notificationInfo = [
@@ -20,39 +28,36 @@ class NotificationService implements NotificationServiceInterface
         ];
         $notificationInfo = json_encode($notificationInfo);
         $cookie = Cookie::get('x-access-token');
-
-        $ch = curl_init();
-
         $options = [
-            CURLOPT_URL            => url(env('NOTIFICATIONS')),
+            CURLOPT_COOKIE         => 'x-access-token=' . $cookie,
             CURLOPT_POSTFIELDS     => $notificationInfo,
             CURLOPT_HEADER         => true,
             CURLOPT_POST           => true,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_COOKIE         => 'x-access-token=' . $cookie,
             CURLOPT_HTTPHEADER     => [
                 'Content-type: application/json',
                 'Content-Length: ' . strlen($notificationInfo)
             ],
         ];
-        curl_setopt_array ($ch , $options);
 
-        $result = curl_exec($ch);
-        $http_status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close ($ch);
-
-        if (empty($result))  {
-            Log::error("Notification request does not receive any response. \n"
-                . 'Curl options: ' . $options);
-        } elseif ($http_status_code != 200) {
-            $info = 'Notification '
-                . $notificationInfo
-                . ' took a response with code '
-                . $http_status_code
-                . ".\n Result: "
-                . $result;
-
+        try {
+            $result = $this->dataGrabber->getRaw(
+                url(env('NOTIFICATIONS')),
+                $options
+            );
+        } catch (RemoteServerException $e) {
+            $info = "Notification request fails. \n"
+                  . $e->getMessage()
+                  . 'Curl options: '
+                  . $options;
             Log::error($info);
+        }
+
+        if (empty($result)) {
+            Log::error(
+                "Notification request does not receive any response. \n"
+                . 'Curl options: '
+                . $options
+            );
         }
     }
 }
