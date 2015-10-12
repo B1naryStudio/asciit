db7942aa6c0c375d2fcfbfdd01471cd9846f3db7
antarus66 <antarus66@gmail.com>
2015-10-13 00:59:32 +0300

    hot-fix-notifications-url: Added a concatenation of prefix to the notifications url.

--- app/Listeners/DeliveryHandlers/HttpHandlers/AnswerToHttp.php
+++ app/Listeners/DeliveryHandlers/HttpHandlers/AnswerToHttp.php
@@ -9,10 +9,13 @@ class AnswerToHttp extends HttpDeliveryHandler
 {
     public function handle(AnswerWasAdded $event)
     {
+        $prefix = env('SERVER_PREFIX', '');
+        $url = url($prefix . '/#questions/' . $event->answer->question->slug);
+
         $this->delivery->send([
             'title' => 'New answer',
-            'text' => 'You have a new answer to your question',
-            'url' => url('/questions/'.$event->answer->question->slug),
+            'text'  => 'You have a new answer to your question',
+            'url'   => $url,
             'users' => [
                 $event->answer->question->user->binary_id
             ]
